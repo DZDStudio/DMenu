56e58697028bedabfcd4e048b924698d994f5ae9
HaiPaya <haipaya@dzdgame.cn>
2024-07-31 11:57:14 +0800

    初始化项目

---
+++ src/main/java/cn/dzdstudo/mc/dmenu/DMenu.java
@@ -0,0 +1,17 @@
+package cn.dzdstudo.mc.dmenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
